2835e34c2607b4fd7f1a6852bc3e8a056e00c8ef
wshilman <walter.shilman@bue.edu.ar>
2020-07-09 23:55:18 -0300

    logging and exceptions

--- comodoro.py
+++ comodoro.py
@@ -1,40 +1,48 @@
 import os
 import time
-from dotenv import load_dotenv
+import logging
 
+from dotenv import load_dotenv
+import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 	
 def main():
-	load_dotenv('.env')
-	print('A empezar a joder...')
-	
+	try:
+		logging.getLogger().setLevel(logging.ERROR)
+		logging.basicConfig(filename='error.log', level=logging.ERROR)
+		load_dotenv('.env')
+		print('A empezar a joder...')
 
-	# Guarda la sesion
-	options = webdriver.ChromeOptions()
-	options.add_argument('--user-data-dir=./SessionData')
-	driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-	driver.get('https://web.whatsapp.com/')
+		# Guarda la sesion
+		options = webdriver.ChromeOptions()
+		options.add_argument('--user-data-dir=./SessionData')
+		driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+		driver.get('https://web.whatsapp.com/')
 
-	name = input('Nombre del usuario o grupo: ')
-	msg = input('Mensaje: ')
-	count = int(input('Cantidad de mensajes: '))
+		name = input('Nombre del usuario o grupo: ')
+		msg = input('Mensaje: ')
+		count = int(input('Cantidad de mensajes: '))
 
-	input('Presione enter una vez escaneado el QR')
+		input('Presione enter para comenzar a enviar.')
 
-	user = driver.find_element_by_xpath(f'//span[contains(@title, "{name}")]')
-	user.click()
-	
-	# The classname of message box may vary.
-	msg_box = driver.find_element_by_class_name(os.getenv('MSG_BOX')) 
+		user = driver.find_element_by_xpath(f'//span[contains(@title, "{name}")]')
+		user.click()
 		
-	for i in range(count):
-		msg_box.send_keys(msg)
-		# The classname of send button may vary.
-		button = driver.find_element_by_class_name(os.getenv('SEND_BUTTON'))
-		button.click()
-		# time.sleep(2)
-	
+		# The classname of message box may vary.
+		msg_box = driver.find_element_by_class_name(os.getenv('MSG_BOX')) 
+			
+		for i in range(count):
+			msg_box.send_keys(msg)
+			# The classname of send button may vary.
+			button = driver.find_element_by_class_name(os.getenv('SEND_BUTTON'))
+			button.click()
+			# time.sleep(2)
+	except selenium.common.exceptions.NoSuchElementException:
+		print("VERIFICAR LOS NOMBRES EN .ENV")
+	except Exception as e:
+		logging.error(e)
+		print('Hubo un error, guardado en el archivo ./error.log')
 	print('DONE.')
 
 main()
