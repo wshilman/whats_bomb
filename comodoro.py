0881838240aee87aa7b4cdbbed7cc6eb883d1c04
wshilman <walter.shilman@bue.edu.ar>
2020-07-10 00:27:49 -0300

    fix: dotenv names

--- comodoro.py
+++ comodoro.py
@@ -8,8 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 	
 def main():
 	try:
+		# Logging
 		logging.getLogger().setLevel(logging.ERROR)
 		logging.basicConfig(filename='error.log', level=logging.ERROR)
+
+		# Variables de entorno
 		load_dotenv('.env')
 		print('A empezar a joder...')
 
@@ -18,26 +21,26 @@ def main():
 		options.add_argument('--user-data-dir=./SessionData')
 		driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 		driver.get('https://web.whatsapp.com/')
-
+		
+		# Datos
 		name = input('Nombre del usuario o grupo: ')
 		msg = input('Mensaje: ')
 		count = int(input('Cantidad de mensajes: '))
 
 		input('Presione enter para comenzar a enviar.')
 
+		# Target
 		user = driver.find_element_by_xpath(f'//span[contains(@title, "{name}")]')
 		user.click()
 		
-		# The classname of message box may vary.
-		msg_box = driver.find_element_by_class_name('sasdasd')
-		# msg_box = driver.find_element_by_class_name(os.getenv('MSG_BOX')) 
+		msg_box = driver.find_element_by_class_name(os.getenv('MSG_BOX')) 
 			
 		for i in range(count):
 			msg_box.send_keys(msg)
-			# The classname of send button may vary.
 			button = driver.find_element_by_class_name(os.getenv('SEND_BUTTON'))
 			button.click()
 			# time.sleep(2)
+			
 	except common.exceptions.NoSuchElementException:
 		print("VERIFICAR LOS NOMBRES EN .ENV")
 	except Exception as e:
